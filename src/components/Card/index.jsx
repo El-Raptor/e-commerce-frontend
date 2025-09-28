605c2b59e7a3e2365dc6ff4d9046848743975999
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-09-28 11:26:52 -0400

    feat(navbar): add navbar and its buttons

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -9,7 +9,7 @@ const Card = () => {
 
   return (
     <>
-      <div className="card" >
+      <div className="card">
         <img src={"https://images7.kabum.com.br/produtos/fotos/925367/iphone-17-apple-256gb-48mp-tela-6-3-super-retina-xdr-preto_1757694916_gg.jpg"} alt="iphone 17" />
         <span className="product-name">Apple iPhone 7 de 256gb - Preto</span>
         <span>7.199,10</span>
